9b5f5a1a828f0950c2108110a709e08f437245f2
Javier Carranza <javishiva22@gmail.com>
2023-09-28 00:41:54 -0300

    STORE subo archivos Android

--- Android/app/src/main/java/org/mywire/temiroapp/MainActivity.java
+++ Android/app/src/main/java/org/mywire/temiroapp/MainActivity.java
@@ -1,7 +1,12 @@
 package org.mywire.temiroapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import org.mywire.temiroapp.ui.store.storeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void Iniciar_store(View view){
+        Intent iniciar_store = new Intent(this, storeActivity.class);
+        startActivity(iniciar_store);
+
+
     }
 }
--- Android/app/src/main/java/org/mywire/temiroapp/ui/product/ProductServicesPpalFragment.java
+++ Android/app/src/main/java/org/mywire/temiroapp/ui/product/ProductServicesPpalFragment.java
@@ -60,14 +60,7 @@ public class ProductServicesPpalFragment extends Fragment {
         }
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
