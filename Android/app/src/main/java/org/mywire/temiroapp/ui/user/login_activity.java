fdb3d8f88df458a5244e1e5930fd1e4012d363bf
Urbina Tatiana <84819160+Tatiana-Urbina@users.noreply.github.com>
2023-09-28 23:53:21 -0300

    Se crea activity login Epica 2 (#104)

---
+++ Android/app/src/main/java/org/mywire/temiroapp/ui/user/login_activity.java
@@ -0,0 +1,20 @@
+package org.mywire.temiroapp.ui.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import org.mywire.temiroapp.MainActivity;
+import org.mywire.temiroapp.R;
+
+public class login_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+
+}
