0a2adfb6e5005018547b25f3518b3bae6b95a62f
acostanoeliacba <acostanoeliacba@outlook.com>
2023-09-30 18:55:57 -0300

    Activitys de Contacto y Turnero

--- Android/app/src/main/java/org/mywire/temiroapp/ui/main/ContactoUno.java
+++ Android/app/src/main/java/org/mywire/temiroapp/ui/main/ContactoUno.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ContactActivity1 extends AppCompatActivity {
+public class ContactoUno extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.contact_activity1);
+        setContentView(R.layout.main_contacto_uno);
     }
 }
