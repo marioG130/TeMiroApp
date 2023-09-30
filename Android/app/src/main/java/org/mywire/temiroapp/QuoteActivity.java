3f687d7aba64c4d26517f8e54387beca9caacf24
Javier Carranza <javishiva22@gmail.com>
2023-09-30 15:49:46 -0300

    creo 3 activitys nuevas

---
+++ Android/app/src/main/java/org/mywire/temiroapp/QuoteActivity.java
@@ -0,0 +1,14 @@
+package org.mywire.temiroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quote_activity);
+    }
+}
