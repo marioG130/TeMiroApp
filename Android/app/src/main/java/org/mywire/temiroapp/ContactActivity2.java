3b71154094a63f6922e3af45b9f3b7cfc9e3bb09
Javier Carranza <106918647+JavierCarranza0101@users.noreply.github.com>
2023-09-30 15:52:30 -0300

    creo 3 activitys nuevas (#116)

---
+++ Android/app/src/main/java/org/mywire/temiroapp/ContactActivity2.java
@@ -0,0 +1,14 @@
+package org.mywire.temiroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_activity2);
+    }
+}
