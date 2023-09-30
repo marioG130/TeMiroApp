ab211e5eb85607ced18fe4260f49459204a9af1f
acostanoeliacba <95321659+acostanoeliacba@users.noreply.github.com>
2023-09-30 18:59:49 -0300

    Activitys de Contacto y Turnero (#117)

--- Android/app/src/main/java/org/mywire/temiroapp/ui/main/ContactoDos.java
+++ Android/app/src/main/java/org/mywire/temiroapp/ui/main/ContactoDos.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ContactActivity2 extends AppCompatActivity {
+public class ContactoDos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.contact_activity2);
+        setContentView(R.layout.main_contacto_dos);
     }
 }
--- Android/app/src/main/java/org/mywire/temiroapp/ui/quote/QuoteTurnero.java
+++ Android/app/src/main/java/org/mywire/temiroapp/ui/quote/QuoteTurnero.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class QuoteActivity extends AppCompatActivity {
+public class QuoteTurnero extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.quote_activity);
+        setContentView(R.layout.quote_turnero);
     }
 }
