ec21ac7ccf95179136b07bf95f7dbf0b94971c6f
Ale <aleprieto@mi.unc.edu.ar>
2023-10-10 20:21:01 -0300

    docu y prueba video

--- Android/app/src/main/java/org/mywire/temiroapp/ui/train/TrainActivity1.java
+++ Android/app/src/main/java/org/mywire/temiroapp/ui/train/TrainActivity1.java
@@ -6,15 +6,18 @@ import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import org.mywire.temiroapp.R;
+import android.net.Uri;
 
 public class TrainActivity1 extends AppCompatActivity {
 
     private Button parpadeoButton;
-    private Button enfoqueButton;
-    private Button movimientosButton;
+    private Button cuerdaButton;
+    private Button movimientoButton;
     private TextView mensajeParpadeo;
-    private TextView mensajeEnfoque;
+    private TextView mensajeCuerda;
     private TextView mensajeMovimientos;
+    private VideoView videoView;
+    private TextView descripcionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,34 +25,81 @@ public class TrainActivity1 extends AppCompatActivity {
         setContentView(R.layout.train_activity);
 
         parpadeoButton = findViewById(R.id.parpadeoButton);
-        enfoqueButton = findViewById(R.id.enfoqueButton);
-        movimientosButton = findViewById(R.id.movimientosButton);
+        enfoqueButton = findViewById(R.id.cuerdaButton);
+        movimientosButton = findViewById(R.id.movimientoButton);
         mensajeParpadeo = findViewById(R.id.mensajeParpadeo);
-        mensajeEnfoque = findViewById(R.id.mensajeEnfoque);
-        mensajeMovimientos = findViewById(R.id.mensajeMovimientos);
+        mensajeEnfoque = findViewById(R.id.mensajeCuerda);
+        mensajeMovimientos = findViewById(R.id.mensajeMovimiento);
+        videoView = findViewById(R.id.videoView);
+        descripcionTextView = findViewById(R.id.descripcionTextView);
 
+        //recordar la carpeta res/raw con los 3 videos
+        // Configurar la fuente del video para Parpadeo
+        final String videoPathParpadeo = "android.resource://" + getPackageName() + "/" + R.raw.Parpadeo_Ejer;
         parpadeoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Mostrar mensaje de construcción para el ejercicio parpadeo
+                // Mostrar mensaje de construcción para el ejercicio Parpadeo
                 mensajeParpadeo.setVisibility(View.VISIBLE);
+                
+                // Configurar el texto de descripción para el ejercicio Parpadeo
+                descripcionTextView.setText(getString(R.string.descripcion_parpadeo));
+                
+                // Configurar la fuente del video y empezar la reproducción
+                videoView.setVideoURI(Uri.parse(videoPathParpadeo));
+                videoView.start();
             }
         });
 
+        // Configurar la fuente del video para cuerda
+        final String videoPathCuerda = "android.resource://" + getPackageName() + "/" + R.raw.Cuerda_Ejer;
         enfoqueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Mostrar mensaje de construcción para el ejercicio de enfoque
-                mensajeEnfoque.setVisibility(View.VISIBLE);
+                // Mostrar mensaje de construcción para el ejercicio Cuerda
+                mensajeCuerda.setVisibility(View.VISIBLE);
+                
+                // Configurar el texto de descripción para el ejercicio Cuerda
+                descripcionTextView.setText(getString(R.string.descripcion_cuerda));
+                
+                // Configurar la fuente del video y empezar la reproducción
+                videoView.setVideoURI(Uri.parse(videoPathCuerda));
+                videoView.start();
             }
         });
 
-        movimientosButton.setOnClickListener(new View.OnClickListener() {
+        // Configurar la fuente del video para Movimientos
+        final String videoPathMovimientos = "android.resource://" + getPackageName() + "/" + R.raw.Movimiento_Ejer;
+        movimientoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Mostrar mensaje de construcción para el ejercicio movimientos
-                mensajeMovimientos.setVisibility(View.VISIBLE);
+                // Mostrar mensaje de construcción para el ejercicio Movimientos
+                mensajeMovimiento.setVisibility(View.VISIBLE);
+                
+                // Configurar el texto de descripción para el ejercicio Movimientos
+                descripcionTextView.setText(getString(R.string.descripcion_movimiento));
+                
+                // Configurar la fuente del video y empezar la reproducción
+                videoView.setVideoURI(Uri.parse(videoPathMovimiento));
+                videoView.start();
             }
         });
+    }
+
+    // Agregar los métodos onPause() y onDestroy() para gestionar el ciclo de vida del VideoView
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Pausar la reproducción del video si está en curso
+        if (videoView.isPlaying()) {
+            videoView.pause();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Liberar recursos asociados al VideoView
+        videoView.stopPlayback();
     }
 }
