04a97af8e4cd5894a7c2c008face8e63cc984cf2
Urbina Tatiana <urbinatatiana077@gmail.com>
2023-10-24 16:07:59 -0300

    Se crean activitys de turnero

---
+++ Android/app/src/main/java/org/mywire/temiroapp/ui/quote/TurnoAgendadoActivity.java
@@ -0,0 +1,16 @@
+package org.mywire.temiroapp.ui.quote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.mywire.temiroapp.R;
+
+public class TurnoAgendadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quote_turno_agendado);
+    }
+}
