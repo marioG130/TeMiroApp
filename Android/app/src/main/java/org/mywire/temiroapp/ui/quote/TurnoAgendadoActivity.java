8eab31d49bc1a6b1e1ae3e9395c8ad9952a141fa
acostanoeliacba <95321659+acostanoeliacba@users.noreply.github.com>
2023-10-24 18:59:46 -0300

    42 tk51 a turnero (#138)
    * Se crean activitys de turnero
    
    * Pantallas del Turnero
    
    ---------
    
    Co-authored-by: Urbina Tatiana <urbinatatiana077@gmail.com>

---
+++ Android/app/src/main/java/org/mywire/temiroapp/ui/quote/TurnoAgendadoActivity.java
@@ -0,0 +1,29 @@
+package org.mywire.temiroapp.ui.quote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.mywire.temiroapp.R;
+
+public class TurnoAgendadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quote_turno_agendado);
+
+        Button btnCancelar = findViewById(R.id.btnCancelar);
+        btnCancelar.setOnClickListener(v -> {
+            Intent act = new Intent(TurnoAgendadoActivity.this, TurnoCanceladoActivity.class);
+            startActivity(act);
+        });
+
+        Button btnVolverHome = findViewById(R.id.btnVolverHome);
+        btnVolverHome.setOnClickListener(v -> {
+            Intent act = new Intent(TurnoAgendadoActivity.this, MainActivity.class);
+            startActivity(act);
+        });
+
+    }
+}
---
+++ Android/app/src/main/java/org/mywire/temiroapp/ui/quote/TurnoCanceladoActivity.java
@@ -0,0 +1,29 @@
+package org.mywire.temiroapp.ui.quote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.mywire.temiroapp.R;
+
+public class TurnoCanceladoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quote_turno_cancelado);
+
+        Button btnNuevaReserva = findViewById(R.id.btnNuevaReserva);
+        btnNuevaReserva.setOnClickListener(v -> {
+            Intent act = new Intent(TurnoCanceladoActivity.this, TurneroActivity.class);
+            startActivity(act);
+        });
+
+        Button btnVolverHome = findViewById(R.id.btnVolverHome);
+        btnVolverHome.setOnClickListener(v -> {
+            Intent act = new Intent(TurnoCanceladoActivity.this, MainActivity.class);
+            startActivity(act);
+        });
+
+    }
+}
